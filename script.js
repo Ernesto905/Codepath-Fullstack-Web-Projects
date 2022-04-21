c5edca8c2272a03c0a5f510c442e27e79c4099bf
Samantha He <79659473+SamanthaCP@users.noreply.github.com>
2022-04-20 20:44:39 -0400

    updating with tdd testing

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 /*
 COMPLETE THE FOLLOWING TASKS WITHOUT MODIFYING ANY OF THE CODE IN THE HTML OR CSS FILE.
 
-You should only write code in the JavaScript functions below, as well as the object with your student information.
+You should only write code in the JavaScript functions below, 
+as well as update the `studentInformation` object with your personal information.
 
 Each function is annotated with a comment explaining what it should do.
 
-By the end of the lab, there shouldn't be any question marks left on the report card.
+By the end of the lab, all tests in the report should be passing.
 */
 
 const studentInformation = {
@@ -57,8 +58,12 @@ const gpaPointsLookup = {
 /**
  * QUERY SELECTORS VARIABLES GO HERE
  */
-const dropdown = document.querySelector(".dropdown")
+const dropdownEl = document.querySelector(".dropdown")
 // ADD more query selectors here
+
+/**
+ * SOLUTIONS FOR STUDENT INFO DOM UPDATES
+ */
 
 /**
  * Modify the report card to display the correct grade level from the lookup table above.
@@ -101,7 +106,7 @@ function updateMajor(studentMajor) {
  *
  * @param {Number} graduationyear - the year the student graduates
  */
-function updateStudentGraduationYear(graduationyear) {
+function updateStudentGraduationYear(graduationYear) {
   // code goes here
 }
 
@@ -118,21 +123,25 @@ function updateStudentImage(imageUrl) {
 /**
  * This function should run as soon as the page loads and update the correct student info
  */
-function populateStudentInfo({ name, grade, advisor, major, graduationYear, imageUrl }) {
-  updateStudentName(name)
-  updateStudentGradeLevel(grade)
-  updateStudentAdvisor(advisor)
-  updateMajor(major)
-  updateStudentGraduationYear(graduationYear)
-  updateStudentImage(imageUrl)
+function populateStudentInfo(studentInformationObject) {
+  updateStudentName(studentInformationObject.name)
+  updateStudentGradeLevel(studentInformationObject.grade)
+  updateStudentAdvisor(studentInformationObject.advisor)
+  updateMajor(studentInformationObject.major)
+  updateStudentGraduationYear(studentInformationObject.graduationYear)
+  updateStudentImage(studentInformationObject.imageUrl)
 }
+
+/**
+ * SOLUTION FOR INNER HTML DOM UPDATES
+ */
 
 /**
  * This function should add a headers row to the report card table
  */
-function addReportCardHeaders() {
+function addReportCardHeaders(reportCardTableElement) {
   // update the code here
-  reportCardTable.innerHTML += ``
+  reportCardTableElement.innerHTML += ``
 }
 
 /**
@@ -142,54 +151,124 @@ function addReportCardHeaders() {
  * @param {Object} course
  * @param {Number} rowNum
  */
-function addCourseRowToReportCard(course, rowNum) {
+function addCourseRowToReportCard(reportCardTableElement, course, rowNum) {
   // update the code here with information about the course passed to this function
-  reportCardTable.innerHTML += `
-    
+  reportCardTableElement.innerHTML += `
+  <div class="table-row course-row row-${rowNum + 1} ${rowNum % 2 === 1 ? "odd" : "even"}">
+
+  </div>
   `
 }
 
-
 /**
- * This is the primary function used to update the report card when the semester changes.
- * It should call the other functions responsible for creating the necessary HTML
+ * This function should add HTML for the totals row in the report card.
  */
-function updateReportCard() {
-  updateDropdownLabel()
-  reportCardTable.innerHTML = ``
-
-  // add your code here
-
-
+function addTotalsRow(reportCardTableElement) {
+  reportCardTableElement.innerHTML += ``
 }
 
 /**
- * This function should toggle the dropdown.
- *
- * If the dropdown classList contains the "closed" class, it should remove it.
- * If the dropdown classList doesn't contain the "closed" class, it should add it.
+ * This function should add HTML for the final row in the report card.
  */
-function toggleDropdown() {
+function addGpaRow(reportCardTableElement) {
+  reportCardTableElement.innerHTML += ``
+}
+
+/**
+ * This is the primary function used to update the report card when the semester changes
+ *
+ * It should call the other functions responsible for creating the necessary HTML
+ *
+ */
+function updateReportCard(reportCardTableElement, currentSemester) {
+  // update the dropdown label
+  updateDropdownLabel()
+  // reset the report card table's inner html to an empty string
+  if (reportCardTableElement) reportCardTableElement.innerHTML = ``
+
+  // add your code here
+}
+
+/**
+ * SOLUTION FOR DROPDOWN EVENT HANDLERS
+ */
+
+/**
+ * These two functions should be used to toggle the dropdown.
+ *
+ * If the dropdown classList contains the "closed" class, the 'openDropdown' function should remove it.
+ * If the dropdown classList doesn't contain the "closed" class, 'closeDropdown' function should add it.
+ */
+function closeDropdown(dropdownElement) {
+  // code goes here
+}
+
+function openDropdown(dropdownElement) {
   // code goes here
 }
 
 /**
  * This function should update the inner html of the dropdown label to be the current
  * value stored in the `semester` variable.
+ *
  */
 function updateDropdownLabel() {
   // code goes here
 }
 
-// Add an event listener for the dropdown button that calls the toggleDropdown button
+/**
+ * This function should add the proper event listeners to the correct DOM elements
+ * responsible for managing the state of the dropdown menu.
+ *
+ */
+function addEventListeners(
+  dropdownElement,
+  dropdownButtonElement,
+  reportCardTableElement,
+  fallSemesterElement,
+  springSemesterElement,
+  winterTermElement
+) {
+  // Add an event listener for the dropdown button that calls the openDropdown function with the correct DOM element
+  // Add 3 event listeners - one for the fall semester option, the spring semester option, and the winter term option
+  // Each callback function one should update the `semester` variable,
+  // call the `updateReportCard` function, and close the dropdown
+}
 
-// Add 3 event listeners - one for the fall semester option, the spring semester option, and the winter term option
-// Each callback function one should update the `semester` variable,
-// call the `updateReportCard` function and toggle the dropdown
+/***************
+   CALCULATIONS
+****************/
 
-/*
-* Add functions here to make it execute as soon as the page loads
-*/
+/**
+ * Use query selectors on the `reportCardTableElement` element
+ * to access the credits the student has earned for each course.
+ * Add them up and display the total in the proper location.
+ *
+ * It may be useful to check for the existence of certain elements
+ * before updating the DOM.
+ *
+ */
+function addUpStudentCredits(reportCardTableElement) {
+  // code goes here
+}
+
+/**
+ * Use query selectors on the `reportCardTableElement` element
+ * to access the letter grades for each course and convert them to GPA points using
+ * the `pointsLookup` object.
+ *
+ * That English Literature grade is keeping us from all A's! Let's see if we can't update that to an A-
+ * since we definitely deserve it after a hard semester of work.
+ *
+ * Then perform a quick calculation and update the report card with the total points and
+ * cumulative grade point average for the semester.
+ *
+ */
+
+function calculateSemesterGpa(reportCardTableElement) {
+  // code goes here
+}
+
 window.onload = function () {
-  // run your function here to make it execute as soon as the page loads
+  // execute your functions here to make sure they run as soon as the page loads
 }
