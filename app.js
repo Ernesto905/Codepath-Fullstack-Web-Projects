900a545ede48ef4fed7c97312b5e3013432789b3
eernesto <eernesto@umich.edu>
2022-06-08 19:53:17 -0400

    Final commit

--- app.js
+++ app.js
@@ -3,30 +3,55 @@ const MY_API_KEY = 'RNtYFS9Q4vYDV1E5LLJsw58nQdlOZReC'
 const limit = 9
 const rating = 'g'
 const lang = 'en'
+let page = 0
+let offset = 0
 
 //reference each item
 let gifResults = document.getElementById('gif_results')
 let submit = document.getElementById('button')
-let searchedGif = document.getElementById('gifSearch').value
+const searchedGif = document.getElementById('gifSearch')
+let addGifsButton = document.getElementById('addGifsBtn')
+
+
+
+console.log(searchedGif)
+
 
 submit.addEventListener('click', getResults)
 
+
 async function getResults(evt) {
-    console.log("I have been pushed")
-
-    apiUrl = "https://api.giphy.com/v1/gifs/search?" + "q=" + searchedGif 
-    + "&limit=" + limit + "&rating=" + rating 
-    +  "&lang=" + lang + "&api_key=" + MY_API_KEY;
-
-    //prevent default
+    //prevent reload 
     evt.preventDefault()
-     
-
+    
+    //replace html elements
+    gifResults.innerHTML = ""
+    
+    let apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=RNtYFS9Q4vYDV1E5LLJsw58nQdlOZReC&rating=g&limit=9&offset=${offset}&q=` + searchedGif.value;
+    
 
     //retrieve data from API URL and turn it into json 
     const response = await fetch(apiUrl)
     const responseData = await response.json()
 
-    console.log(responseData)
+    let images = responseData.data
+
+    images.forEach(element => {
+        displayGifs(element)
+    });
+
+    searchedGif.value = ""
 
 }
+
+function displayGifs (gif) {
+    let ourImage = gif.images.downsized.url
+    
+    ourHTML = '<img class=individual-gif src=' + ourImage + ' alt=Testing>'
+
+    gifResults.innerHTML += ourHTML
+
+}
+
+
+
