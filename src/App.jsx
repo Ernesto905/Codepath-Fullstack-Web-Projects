6341314007f80a67c11fd16cbd0c47baf9ef7200
eernesto <eernesto@umich.edu>
2022-06-14 17:37:25 -0400

    first two stretch features implemented. Saving codebase before attempting the third

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import * as React from "react"
 // IMPORT ANY NEEDED COMPONENTS HERE
 import { createDataSet } from "./data/dataset"
 import "./App.css"
+import Header from "./components/Header/Header"
+import Instructions from "./components/Instructions/Instructions"
+import Chip from './components/Chip/Chip'
+import NutritionalLabel from "./components/NutritionalLabel/NutritionalLabel"
+import { useState } from "react"
 
 // don't move this!
 export const appInfo = {
@@ -21,6 +26,71 @@ export const appInfo = {
 const { data, categories, restaurants } = createDataSet()
 
 export function App() {
+
+  let closedCat=false;
+  let closedRest=false;
+  let closedItem=false;
+  //states
+
+  const [cat, setCat] = useState(null)
+  const [rest, setRest] = useState(null)
+  const [ourItem, setItem] = useState(null)
+
+  //event handlers
+  function closeCat() {
+    closedCat=true;
+    setCat(null)
+  }
+  function closeRest() {
+    closedRest=true;
+    setRest(null)
+  }
+  function closeItem() {
+    closedItem=true;
+    setItem(null)
+  }
+  function handleCatChange(category) {
+    if(!closedCat)
+      setCat(category)
+  }
+  function handleRestChange(restaurant) {
+    if(!closedRest)
+      setRest(restaurant)
+  }
+  function handleItemChange(item) {
+    if(!closedItem)
+      setItem(item)
+  }
+
+  function decideInstructions() {
+    
+    if(cat==null && rest==null && ourItem==null){
+      
+      return (appInfo.instructions.start)
+    }
+    else if(cat!=null && rest == null){
+      
+      return(appInfo.instructions.onlyCategory)
+    }
+    else if (cat==null && rest!= null) {
+      return (appInfo.instructions.onlyRestaurant)
+      
+    }
+    else if (cat!=null && rest!= null && ourItem==null) {
+      
+      return(appInfo.instructions.noSelectedItem)
+    }
+    else {
+      return(appInfo.allSelected)
+    }
+    
+  }
+
+  const currentMenuItems = data.filter((item) => 
+    {return item.food_category === cat && item.restaurant === rest}
+    )
+
+
   return (
     <main className="App">
       {/* CATEGORIES COLUMN */}
@@ -28,30 +98,64 @@ export function App() {
         <div className="categories options">
           <h2 className="title">Categories</h2>
           {/* YOUR CODE HERE */}
+          {categories.map((category, index) => 
+          <Chip label={category} 
+            key={category + '-' + index} 
+            onClick={() => handleCatChange(category)} 
+            onClose={closeCat}
+            isActive={cat === category}
+          />)
+          } 
+          
         </div>
       </div>
 
       {/* MAIN COLUMN */}
       <div className="container">
         {/* HEADER GOES HERE */}
-
+        <Header title={appInfo.title} tagline={appInfo.tagline} description={appInfo.description}/>
         {/* RESTAURANTS ROW */}
         <div className="RestaurantsRow">
           <h2 className="title">Restaurants</h2>
-          <div className="restaurants options">{/* YOUR CODE HERE */}</div>
+          <div className="restaurants options">
+            {restaurants.map((restaurant, index) => 
+            <Chip 
+            label={restaurant} 
+            key={restaurant + '-' + index}
+            onClick={() => handleRestChange(restaurant)}
+            onClose={closeRest}
+            isActive={restaurant === rest}
+            />
+            )}
+          </div>
         </div>
 
         {/* INSTRUCTIONS GO HERE */}
+        <Instructions instructions={decideInstructions()}/>
 
         {/* MENU DISPLAY */}
         <div className="MenuDisplay display">
           <div className="MenuItemButtons menu-items">
             <h2 className="title">Menu Items</h2>
             {/* YOUR CODE HERE */}
+            {currentMenuItems.map((item, index) => 
+              <Chip 
+              label={item.item_name}
+              key={item.item_name + '-' + index}
+              onClick={() => handleItemChange(item)}
+              onClose={closeItem}
+              isActive={item===ourItem}
+              />
+            )}
           </div>
 
           {/* NUTRITION FACTS */}
-          <div className="NutritionFacts nutrition-facts">{/* YOUR CODE HERE */}</div>
+          <div className="NutritionFacts nutrition-facts">
+
+              {ourItem ? <NutritionalLabel item={ourItem}/> : null}
+              
+
+          </div>
         </div>
 
         <div className="data-sources">
--- src/components/NutritionalLabel/NutritionalLabel.jsx
+++ src/components/NutritionalLabel/NutritionalLabel.jsx
@@ -3,13 +3,27 @@ import { nutritionFacts } from "../../constants"
 import "./NutritionalLabel.css"
 
 export function NutritionalLabel(props) {
+  const {item} = props
+  
   return (
     <div className="nutritional-label">
       <h3 className="title">Nutrition Facts</h3>
 
-      <h4 className="item-name">{`CHANGE_ME`}</h4>
+      <h4 className="item-name">{props.item?.item_name}</h4>
 
-      <ul className="fact-list">{/* WRITE CODE HERE */}</ul>
+      <ul className="fact-list">
+        {nutritionFacts.map((fact) => 
+          
+          {return <NutritionalLabelFact
+          key={fact.id}
+          label={fact.label}
+          attribute={fact.attribute} /* we want to pass in the props.item.(fact.label as a string) */
+          item={item}
+    
+          />}
+
+        )}
+      </ul>
     </div>
   )
 }
@@ -17,8 +31,9 @@ export function NutritionalLabel(props) {
 export function NutritionalLabelFact(props) {
   return (
     <li className="nutrition-fact">
-      <span className="fact-label">{/* WRITE CODE HERE */}</span>{" "}
-      <span className="fact-value">{/* WRITE CODE HERE */}</span>
+      
+      <span className="fact-label">{props.label}</span>{" "}
+      <span className="fact-value">{props.item[props.attribute]}</span>
     </li>
   )
 }
